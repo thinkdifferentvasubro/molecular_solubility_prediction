896bedef199027297fbf724e6f71fa8b95d5b5ba
thinkdifferentvasubro <thinkdifferentvasubro@gmail.com>
2026-05-12 20:19:06 +0530

    initial commit

--- src/serving/plot.py
+++ src/serving/plot.py
@@ -6,10 +6,10 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src.Data.load import load_data
 
 
-def build_plot(smiles, solubility_predicted):
-    train, _, _ = load_data()
-    measured_solubility = train["measured log solubility in mols per litre"].values
+train, _, _ = load_data()
+measured_solubility = train["measured log solubility in mols per litre"].values
 
+def build_plot(smiles, solubility_predicted):
     fig, ax = plt.subplots(figsize=(8, 5))
 
     ax.hist(
